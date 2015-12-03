706dc94dfc34fd98cf70a35c4fc5876e98177ace
yannlandry <yann.landry.94@gmail.com>
2015-12-03 18:09:38 -0500

    Fix things

---
+++ src/simModel/LoadUnloadWaitingLine.java
@@ -0,0 +1,12 @@
+package simModel;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class LoadUnloadWaitingLine {
+	// A fixed size list of the identifiers of all the sampleHolder awaiting to be serviced by the load/unload area
+	// private, use methods instead
+	Queue<Integer> loadUnloadWaitingLine = new ArrayBlockingQueue<Integer>(Constants.LUA_Q_LEN);
+
+	// Keep track of the # of empty sample holders
+	int numEmptyHolders;
+}
