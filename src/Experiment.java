47f311529c53b445a4119c036d03460912e269b4
Samira El-Rayyes <samira.elrayyes@gmail.com>
2015-12-03 23:21:03 -0500

    sorta cleaned output display in Experiment.java

--- src/Experiment.java
+++ src/Experiment.java
@@ -12,7 +12,7 @@ class Experiment
    public static void main(String[] args)
    {
        int i = 0; 
-       int maxSampleHoldersWaiting;
+       int maxSampleHoldersWaiting = 5;
        double startTime=0.0, endTime=1440.0;
        SMLabTesting labTesting;  // Simulation object
 
@@ -20,41 +20,27 @@ class Experiment
        RandomSeedGenerator rsg = new RandomSeedGenerator();
        Seeds sds = new Seeds(rsg);
        
-       /* Case 1 (base case): maxNumSampleHolders = 5 */
-       maxSampleHoldersWaiting = 5;
-       labTesting = new SMLabTesting(startTime,endTime,maxSampleHoldersWaiting,sds);
-       labTesting.runSimulation();
+       /* For each case, run simulation and display the output
+       Case 1 (base case): maxNumSampleHolders = 5 
+       Case 2: maxSampleHoldersWaiting = 4
+       Case 3: maxSampleHoldersWaiting = 3
+       Case 4: maxSampleHoldersWaiting = 2
+       Case 5: maxSampleHoldersWaiting = 1
+       */
+       for(i = maxSampleHoldersWaiting; i > 0; i--) {
+    	   labTesting = new SMLabTesting(startTime,endTime,i,sds);
+           labTesting.runSimulation();
+           
+           // Display output
+           // Display percent normal and rush samples completed
+           System.out.print("Case " + (maxSampleHoldersWaiting-i+1) + "\tPctNormalSamplesCompleted = " + labTesting.getPctNormalSamplesCompleted()
+           + "\tPctRushSamplesCompleted = " + labTesting.getPctRushSamplesCompleted());
+           
+           // Display percent unsuccessful entry for each test cell
+           for(int j = 0; j < 5; j++)
+        	   System.out.print("Test Cell " + (j+1) + "\tPctUnsuccesfulEntry = " + labTesting.getPctUnsuccessfulEntry()[j]);
+           System.out.println();
+       }
        
-       // Display output
-       // Display percent normal and rush samples completed
-       System.out.print("Sample: " + (i+1) + "\tPctNormalSamplesCompleted = " + labTesting.getPctNormalSamplesCompleted()
-       + "\tPctRushSamplesCompleted = " + labTesting.getPctRushSamplesCompleted());
-       
-       // Display percent unsuccessful entry for each test cell
-       for(int j = 0; j < 5; j++)
-    	   System.out.print("Test Cell " + (j+1) + " PctUnsuccesfulEntry = " + labTesting.getPctUnsuccessfulEntry()[j]);
-       System.out.println();
-       
-       /* Case 2: maxSampleHoldersWaiting = 4 */
-       maxSampleHoldersWaiting = 4;
-       labTesting = new SMLabTesting(startTime,endTime,maxSampleHoldersWaiting,sds);
-       labTesting.runSimulation();
-       
-       /* Case 3: maxSampleHoldersWaiting = 3 */
-       maxSampleHoldersWaiting = 3;
-       labTesting = new SMLabTesting(startTime,endTime,maxSampleHoldersWaiting,sds);
-       labTesting.runSimulation();
-
-       /* Case 4: maxSampleHoldersWaiting = 2 */
-       maxSampleHoldersWaiting = 2;
-       labTesting = new SMLabTesting(startTime,endTime,maxSampleHoldersWaiting,sds);
-       labTesting.runSimulation();
-       
-       /* Case 5: maxSampleHoldersWaiting = 1 */
-       maxSampleHoldersWaiting = 1;
-       labTesting = new SMLabTesting(startTime,endTime,maxSampleHoldersWaiting,sds);
-       labTesting.runSimulation();
-
-
    }
 }
